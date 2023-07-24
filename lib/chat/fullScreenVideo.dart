0b80460a4c3567eac71943c241bc72e38797ec2a
Jonas Boateng <jonas.boateng@ellogroup.co.uk>
2023-07-24 08:35:43 +0100

    Should fix the video fullscreen mode

--- lib/chat/fullScreenVideo.dart
+++ lib/chat/fullScreenVideo.dart
@@ -9,8 +9,23 @@ class FullScreenVideoView extends StatelessWidget {
   Widget build(BuildContext context) {
     var screen = MediaQuery.of(context).size;
 
-    return SizedBox.fromSize(
-      size: Size(screen.width, screen.height * 0.65), // Image border
-      child: ChewieVideoWidget(autoPlayAndFullscreen: false, url: url, file: null,),);
+    return Scaffold(
+        backgroundColor: Colors.black,
+        appBar: AppBar(
+            flexibleSpace:Container(
+              decoration: const BoxDecoration(
+                gradient: LinearGradient(
+                  colors: [Colors.black, Colors.black],
+                  begin: Alignment.centerLeft,
+                  end: Alignment.centerRight,
+                  stops: [0.2, 0.9],
+                ),
+              ),
+            ),
+        ),
+        body: Center(child: SizedBox.fromSize(
+          size: Size(screen.width, screen.height * 0.65), // Image border
+          child: ChewieVideoWidget(autoPlayAndFullscreen: false, url: url, file: null,),),)
+    );
   }
 }
